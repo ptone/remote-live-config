4ebda419b9e344d16be790cfb0edd9725637aec3
Preston Holmes <preston@ptone.com>
2019-07-21 13:24:04 +0000

    format node example

--- nodejs-live-read-example/index.js
+++ nodejs-live-read-example/index.js
@@ -2,15 +2,16 @@ var SelfReloadJSON = require('self-reload-json');
 
 var config = new SelfReloadJSON('/tmp/demo.json');
 
-config.on('updated' , (j) => console.log('config updated'))
+config.on('updated', (j) => console.log('config updated'))
 
 function checkConfig() {
-    console.log(config.msg);
-  };
-  
-  function main() {
-    setInterval(checkConfig, 1000);
-  };
-  
-  main();
+  // some task that uses config values
+  console.log(config.msg);
+};
+
+function main() {
+  setInterval(checkConfig, 1000);
+};
+
+main();
 
